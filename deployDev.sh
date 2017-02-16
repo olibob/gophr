e940a20bfedd039c25d6dc0837397f69ed979670
Olivier Robert <robby57@gmail.com>
2017-02-16 21:14:21 +0100

    fix stop task error 'taskId longer than 36'

--- deployDev.sh
+++ deployDev.sh
@@ -24,7 +24,7 @@ if [ "$SERVICES" == "" ]; then
   echo "entered existing service"
   DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .services[].desiredCount`
     aws ecs update-service --cluster ${CLUSTER} --region ${REGION} --service ${SERVICE_NAME} --task-definition ${FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT}
-    CURRENT_TASK=`aws ecs list-tasks --family ${FAMILY} --cluster ${CLUSTER} | jq '.taskArns[0]'`
+    CURRENT_TASK=`aws ecs list-tasks --family ${FAMILY} --cluster ${CLUSTER} | jq '.taskArns[0]' | tr -d "\""`
     aws ecs stop-task --task ${CURRENT_TASK} --cluster ${CLUSTER}
 else
   echo "entered new service"
