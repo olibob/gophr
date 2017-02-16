2623b75ce4916ac2af5d68aa69f740d3aef34d9f
Olivier Robert <robby57@gmail.com>
2017-02-16 20:49:05 +0100

    dummy change

--- deployDev.sh
+++ deployDev.sh
@@ -24,6 +24,8 @@ if [ "$SERVICES" == "" ]; then
   echo "entered existing service"
   DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .services[].desiredCount`
     aws ecs update-service --cluster ${CLUSTER} --region ${REGION} --service ${SERVICE_NAME} --task-definition ${FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT}
+    CURRENT_TASK=`aws ecs list-tasks --family ${FAMILY} --cluster ${CLUSTER} | jq '.taskArns[0]' | tr -d "\""`
+    aws ecs stop-task --task ${CURRENT_TASK}
 else
   echo "entered new service"
     aws ecs create-service --service-name ${SERVICE_NAME} --desired-count 1 --task-definition ${FAMILY} --cluster ${CLUSTER} --region ${REGION}
