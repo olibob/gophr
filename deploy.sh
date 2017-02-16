ea75b44f9d277c7fed0d3cc8251a810ac1e90476
Olivier Robert <robby57@gmail.com>
2017-02-16 16:12:19 +0100

    Revert "add non dev deployment based on ELB"
    This reverts commit aec2bfd6af59f072c6aad53eb796605b5bab92f9.

--- deploy.sh
+++ deploy.sh
@@ -7,15 +7,6 @@ CLUSTER="apcluster"
 FAMILY=`sed -n 's/.*"family": "\(.*\)",/\1/p' taskdef.json`
 NAME=`sed -n 's/.*"name": "\(.*\)",/\1/p' taskdef.json`
 SERVICE_NAME="${NAME}-service-${ENV_TYPE}"
-TARGET_GROUP_NAME="gophrApp"
-
-getTargetGroupARN() {
-  echo `aws elbv2 describe-target-groups --names ${TARGET_GROUP_NAME} | jq .TargetGroups[].TargetGroupArn  | tr -d '"'`
-}
-
-getLoadBalancerARN() {
-  echo `aws elbv2 describe-target-groups --names ${TARGET_GROUP_NAME} | jq .TargetGroups[].LoadBalancerArns[0] | tr -d '"'`
-}
 
 # Attention!!!
 # The ENV_TYPE variable needs to be defined prior to running this script
@@ -33,11 +24,8 @@ sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_
 #Register the task definition in the repository
 aws ecs register-task-definition --family ${FAMILY} --cli-input-json file://${WORKSPACE}/${NAME}-v_${BUILD_NUMBER}.json --region ${REGION}
 SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]`
-
-#Get latest revision if a revision is not explicitly requested (mainly for not dev deployments)
-if [ ${REVISION} == "" ]; then
-  REVISION=`aws ecs describe-task-definition --task-definition ${FAMILY} --region ${REGION} | jq .taskDefinition.revision`
-fi
+#Get latest revision
+REVISION=`aws ecs describe-task-definition --task-definition ${FAMILY} --region ${REGION} | jq .taskDefinition.revision`
 
 #Create or update service
 if [ "$SERVICES" == "" ]; then
@@ -49,12 +37,6 @@ if [ "$SERVICES" == "" ]; then
       aws ecs update-service --cluster ${CLUSTER} --region ${REGION} --service ${SERVICE_NAME} --task-definition ${FAMILY}:${REVISION} --desired-count ${DESIRED_COUNT}
     else
       DESIRED_COUNT="2"
-      TARGET_GROUP=$(getTargetGroup)
-      LOAD_BALANCER=$(getLoadBalancerARN)
-
-      sed -e "s;%CLUSTER%;${CLUSTER};g" -e "s;%SERVICE_NAME%;${SERVICE_NAME};g" -e "s;%TARGET_GROUP%;${TARGET_GROUP};g" -e "s;%LOAD_BALANCER%;${LOAD_BALANCER};g" servicedef.json > ${SERVICE_NAME}-v_${BUILD_NUMBER}.json
-
-      aws ecs update-service --region ${REGION} --service ${SERVICE_NAME} --cli-input-json file://${SERVICE_NAME}-v_${BUILD_NUMBER}.json
     fi
   fi
 else
@@ -63,11 +45,5 @@ else
     aws ecs create-service --service-name ${SERVICE_NAME} --desired-count 1 --task-definition ${FAMILY} --cluster ${CLUSTER} --region ${REGION}
   else
     echo "deploying ..."
-    TARGET_GROUP=$(getTargetGroup)
-    LOAD_BALANCER=$(getLoadBalancerARN)
-
-    sed -e "s;%CLUSTER%;${CLUSTER};g" -e "s;%SERVICE_NAME%;${SERVICE_NAME};g" -e "s;%TARGET_GROUP%;${TARGET_GROUP};g" -e "s;%LOAD_BALANCER%;${LOAD_BALANCER};g" servicedef.json > ${SERVICE_NAME}-v_${BUILD_NUMBER}.json
-
-    aws ecs create-service --service-name ${SERVICE_NAME} --region ${REGION} --cli-input-json file://${SERVICE_NAME}-v_${BUILD_NUMBER}.json
   fi
 fi
